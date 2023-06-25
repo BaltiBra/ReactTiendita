bddf17f746fa0366de2de1f2aaf5030019c121cb
baltasar <baltibranchesi@live.com.ar>
2023-06-25 17:51:57 -0300

    6tarde

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import './App.css';
 // import Main from './components/Main';
+import { useState } from 'react';
 import NavBar from './components/Navbar';
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from './components/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import fondo from './images/fondo.jpg';
+import fondo from './components/images/fondo.jpg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Libros from './components/Libros';
 import Cositas from './components/Cositas';
 import Main from './components/Main';
 import Carrito from './components/Carrito';
 import Error404 from './components/Error404';
+import SearchResultContainer from './components/SearchResultContainer';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
   return (< div style={{ backgroundImage: `url(${fondo})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', minHeight: '100vh',  overflow: 'hidden'}}>
-    
+        
         <Router>
           <div >
-            < NavBar />
+            < NavBar onSearch={handleSearch}/>
             </div>
           <Routes>
             <Route path='/' index element={<Main/>}/>
             <Route path='/Libros' element={<Libros></Libros>}/>
             <Route path='/Cositas'  element={<Cositas></Cositas>}/>
             <Route path='/*' element={<Error404></Error404>}/>
+            <Route path="/search/:term" Component={SearchResultContainer} />
           </Routes>
           <Carrito></Carrito>
-      <ItemListContainer nombre={'Baltasar'} />
+      <ItemListContainer nombre={'Baltasar'} searchTerm={searchTerm} />
           <ItemDetailContainer/>
   </Router>
  </div> );
